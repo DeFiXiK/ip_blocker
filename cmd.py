25140eaf265b0e2701e830a30d0670deb264f3ee
Oleg Yamnikov <bgeyts667@gmail.com>
2019-01-07 19:36:50 +0500

    Update iptables command

--- cmd.py
+++ cmd.py
@@ -1,7 +1,7 @@
 TCPDUMP = "tcpdump -i {iface} -n -x src not {hostname}"
 
 IPTABLES_CLEAR = "iptables -F"
-IPTABLES_ADD_BLOCK_RULE = "iptables -A INPUT -m state --state NEW -m set --set badip src -j DROP"
+IPTABLES_ADD_BLOCK_RULE = "iptables -A INPUT -m state --state NEW -m set --match-set badip src -j DROP"
 
 IPSET_CHECK = "ipset test badip {ip}"
 IPSET_ADD = "ipset add badip {ip}"
